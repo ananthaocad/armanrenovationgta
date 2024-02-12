abedc04c95f91b1a66e33ba1d22e4636ad1790fe
ananthaocad <151218192+ananthaocad@users.noreply.github.com>
2024-02-12 01:10:28 -0500

    l

--- src/partials/Contact.jsx
+++ src/partials/Contact.jsx
@@ -5,14 +5,8 @@ const Contact = () => {
     <section id="contact" className="py-40 pb-10 bg-white">
       <div className="container mx-auto">
         <h2 className="h2 opacity-20 mb-10 px-5 text-center">Submit a Query</h2>
-        <iframe 
-          src="https://docs.google.com/forms/d/e/1FAIpQLSdsaM0_-iR2u4py8xvS-pWN2qncdA5gUdM5rnNYe7VHEYROwQ/viewform?embedded=true" 
-          width="100%" 
-          height="800" 
-          frameborder="0" 
-          marginheight="0" 
-          marginwidth="0"
-        >
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdsaM0_-iR2u4py8xvS-pWN2qncdA5gUdM5rnNYe7VHEYROwQ/viewform?embedded=true" width="640" height="682" frameborder="0" marginheight="0" marginwidth="0">Loading…
+        </iframe>
           Loading…
         </iframe>
       </div>
